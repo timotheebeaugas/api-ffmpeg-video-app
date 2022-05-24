5907e37e56170fcd995c67818cee6d7f5fb99752
Your Name <you@example.com>
2022-05-24 20:00:57 +0200

    subprocess

--- app.js
+++ app.js
@@ -1,90 +1,122 @@
 const express = require("express");
 const app = express();
 const port = 3011;
-const multer  = require('multer');
-const fs = require('fs');
+const multer = require("multer");
+const fs = require("fs");
+const { spawn } = require("child_process");
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, 'tmp/')
+    callback(null, "tmp/");
   },
   filename: function (req, file, callback) {
-    callback(null, Date.now() + '.' + 'mp4')
-  }
-})
+    callback(null, Date.now() + "." + "mp4");
+  },
+});
 
-function fileFilter (req, file, callback) {
-  if(file.mimetype === 'video/mp4'){
-    callback(null, true)
-  }else{
-    callback(new Error('Wrong minetype')) 
+function fileFilter(req, file, callback) {
+  if (file.mimetype === "video/mp4") {
+    callback(null, true);
+  } else {
+    callback(new Error("Wrong minetype"));
   }
 }
 
 const maxSize = 10 * 1024 * 1024; // 10 Mo
 
-const upload = multer({ storage: storage, limits: { fileSize: maxSize }, fileFilter }).single('file')
-
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: maxSize },
+  fileFilter,
+}).single("file");
 
 // middleware
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
 app.use(function (req, res, next) {
-  if(req.url.startsWith('/video')){
+  if (req.url.startsWith("/video")) {
     next();
-  }else{
-    res.status(400).json({msg: 'Bad request'});
+  } else {
+    res.status(400).json({ msg: "Bad request" });
   }
 });
 
-
 // routes
-app.post('/video', function (req, res) {
-  upload(req, res, function (err) { 
-    if (err instanceof multer.MulterError) { 
-      res.status(500); 
+app.post("/video", function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(500);
     } else if (err) {
-      res.status(400).json({msg: 'Bad request'});
+      res.status(400).json({ msg: "Bad request" });
     }
-    res.status(200).json({msg: 'File uploaded', file: req.file.filename}); 
-  }) 
-})
- 
+
+    const subprocess = spawn("ffprobe", [
+      "-v",
+      "error",
+      "-show_entries",
+      "format=duration",
+      "-of",
+      "default=noprint_wrappers=1:nokey=1",
+      "tmp/1652797178621.mp4",
+    ]);
+
+    subprocess.stdout.on('data', function (data) {
+      let totalDuration = parseFloat(data)
+      res.status(200).json({ msg: "File uploaded", file: req.file.filename, duration: totalDuration });
+    });
+
+    subprocess.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+
+    subprocess.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+
+  });
+});
+
 app.get("/video/:id", (req, res) => {
   const range = req.headers.range;
   if (!range) {
-      res.status(400).send("Requires Range header");
+    res.status(400).send("Requires Range header");
   }
-  const videoPath = 'tmp/' + req.params.id;
+  const videoPath = "tmp/" + req.params.id;
   const videoSize = fs.statSync(videoPath).size;
   const CHUNK_SIZE = 10 ** 6;
   const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1); 
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
   const contentLength = end - start + 1;
   const headers = {
-      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": contentLength,
-      "Content-Type": "video/mp4",
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
   };
   res.writeHead(206, headers);
   const videoStream = fs.createReadStream(videoPath, { start, end });
   videoStream.pipe(res);
 });
 
-app.delete('/video/:id', (req,res) => {
-  const videoPath = 'tmp/' + req.params.id;
-  fs.unlink(videoPath, () => {})
-  res.status(200)
+app.delete("/video/:id", (req, res) => {
+  const videoPath = "tmp/" + req.params.id;
+  fs.unlink(videoPath, () => {});
+  res.status(200);
 });
 
-// server  
+// server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});   
+});
